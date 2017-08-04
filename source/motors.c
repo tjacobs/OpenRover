b3207583b9abc04f71aa1c418efd3ecd85c75d6e
Rover <debian@beaglebone.localdomain>
2017-08-04 07:29:40 +0000

    Motors PWM from C

--- source/motors.c
+++ source/motors.c
@@ -1,14 +1,17 @@
 #include <rc_usefulincludes.h> 
 #include <roboticscape.h>
 
+int speed;
+
 /*******************************************************************************
 * void on_pause_released() 
 *	
 * Make the pause button toggle between paused and running states.
 *******************************************************************************/
 void on_pause_released(){
+	speed = 1000 + rand()%800;
 	// Toggle betewen paused and running modes
-	if(rc_get_state()==RUNNING)		rc_set_state(PAUSED);
+	if(rc_get_state()==RUNNING)	rc_set_state(PAUSED);
 	else if(rc_get_state()==PAUSED)	rc_set_state(RUNNING);
 	return;
 }
@@ -20,8 +23,8 @@ void on_pause_released(){
 * triggers the rest of the program to exit cleanly.
 *******************************************************************************/
 void on_pause_pressed(){
-	int i=0;
-	const int samples = 100;	 // Check for release 100 times in this period
+	int i = 0;
+	const int samples = 100;     // Check for release 100 times in this period
 	const int us_wait = 2000000; // 2 seconds
 	
 	// Keep checking to see if the button is still held down
@@ -35,62 +38,55 @@ void on_pause_pressed(){
 }
 
 void mForward() {
-	rc_set_motor_all(1.0);
-	printf("Moving Forward/n");
+	//rc_send_esc_pulse_normalized(1, 0.3);
+	rc_send_servo_pulse_us(1, speed);
+	printf( "Speed: %d", speed);
+	rc_send_servo_pulse_us(2, 1750);
+	printf("Moving Forward\n");
 }
 
 void mBack() {
-	rc_set_motor_all(-1.0);
-	printf("Moving Back/n");
+	printf("Moving Back\n");
 }
 
 void mStop() {
-	rc_set_motor_all(0.0);
-	printf("Stopping");
+	//rc_send_esc_pulse_normalized(1, 0.0);
+	rc_send_servo_pulse_us(1, 1500);
+	rc_send_servo_pulse_us(2, 1400);
+	printf("Stopping\n");
 }
 
 void mLeft() {
-	rc_set_motor_all(0.0);
-	usleep(500000);
-	rc_set_motor(1,1);
-	rc_set_motor(2,-1);
-	printf("Moivng Left");
+	printf("Moivng Left\n");
 }
 
 void mRight() {
-	rc_set_motor_all(0.0);
-	usleep(500000);
-	rc_set_motor(1,-1);
-	rc_set_motor(2,1);
-	printf("Moving Right");
+	printf("Moving Right\n");
 }
 
 /*******************************************************************************
 * int main() 
 *
-* This template main function contains these critical components
 * - call to rc_initialize() at the beginning
 * - main while loop that checks for EXITING condition
 * - rc_cleanup() at the end
 *******************************************************************************/
 int main(){
+	printf("\nRunning.\n");
+	
 	// Initialize cape library first
 	if(rc_initialize()){
 		fprintf(stderr,"ERROR: failed to initialize rc_initialize(), did you run with sudo?\n");
 		return -1;
 	}
 
-	printf("\nHello BeagleBone\n");
+	rc_enable_servo_power_rail();
 	rc_set_pause_pressed_func(&on_pause_pressed);
 	rc_set_pause_released_func(&on_pause_released);
-
 	rc_set_state(RUNNING); 
 	
-	// Take H-bridges out of standby
-	rc_enable_motors(); 
-
 	// Keep looping until state changes to EXITING
-	while(rc_get_state()!=EXITING){
+	while(rc_get_state() != EXITING){
 		if(rc_get_state()==RUNNING){
 			rc_set_led(GREEN, ON);
 			rc_set_led(RED, OFF);
@@ -104,9 +100,8 @@ int main(){
 		usleep(100000);
 	}
 
-	// User must have existed, put H-bridges into standby
-	rc_disable_motors();	
 	printf("All motors off.\n\n");
+	rc_disable_servo_power_rail();
 	rc_cleanup(); 
 	return 0;
 }
