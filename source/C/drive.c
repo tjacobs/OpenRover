2d30f047c92bc2d4b66ba446540d209278986d2b
Tom Jacobs <tjacobs@expensify.com>
2017-08-18 21:28:50 +1000

    Start on C OpenRover

---
+++ source/C/drive.c
@@ -0,0 +1,26 @@
+#include <iostream>
+#include <cv.h>
+#include <highgui.h>
+ 
+using namespace std;
+char key;
+
+int main(){
+	printf("\nStarting OpenRover.\n");
+    cvNamedWindow("Camera_Output", 1);    // Create window
+    CvCapture* capture = cvCaptureFromCAM(CV_CAP_ANY);  // Capture using any camera connected to your system
+    while(1){
+
+        IplImage* frame = cvQueryFrame(capture); // Create image frames from capture
+        cvShowImage("Camera_Output", frame);   // Show image frames on created window
+
+        key = cvWaitKey(10);
+        if (char(key) == 27){
+            break;
+        }
+    }
+    cvReleaseCapture(&capture); // Release capture
+    cvDestroyWindow("Camera_Output"); // Destroy window
+    return 0;
+}
+
