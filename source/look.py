a1dc617a3265fed4e7a2e29fbd0bacb5ba50dbdf
Rover <hatsmagee@gmail.com>
2017-08-24 04:54:36 +0000

    Face detection

--- source/look.py
+++ source/look.py
@@ -11,8 +11,11 @@ from threading import Thread
 # Start camera
 camera.startCamera((320, 240), 6)
 
-# Create face detector
-face_cascade = cv2.CascadeClassifier('haarcascade_frontalface_default.xml')
+# Create face detectors
+face_cascade1 = cv2.CascadeClassifier('haarcascade_frontalface_default.xml')
+face_cascade2 = cv2.CascadeClassifier('haarcascade_frontalface_alt.xml')
+face_cascade3 = cv2.CascadeClassifier('haarcascade_frontalface_alt2.xml')
+face_cascade4 = cv2.CascadeClassifier('haarcascade_profileface.xml')
 
 # Create window
 display = False
@@ -34,9 +37,22 @@ def timing(f):
 @timing
 def detect_faces(frame):
     print("Detecting faces")
-    global face_cascade
+    global face_cascade1, face_cascade2, face_cascade3, face_cascade4
     gray = cv2.cvtColor(frame, cv2.COLOR_BGR2GRAY)
-    faces = face_cascade.detectMultiScale(gray, 1.3, 5)
+    faces1 = face_cascade1.detectMultiScale(gray, 1.3, 5)
+    faces2 = face_cascade2.detectMultiScale(gray, 1.3, 5)
+    faces3 = face_cascade3.detectMultiScale(gray, 1.3, 5)
+    faces4 = face_cascade4.detectMultiScale(gray, 1.3, 5)
+    faces = []
+#    for face_location in faces1:
+#        faces += face_location
+#    for face_location in faces2:
+#        faces += face_location
+#    for face_location in faces3:
+#        faces += face_location
+#    for face_location in faces4:
+#        faces += face_location
+    faces = faces4
     print( faces )
     return faces
 
@@ -48,32 +64,36 @@ frame = None
 face_locations = []
 face_x = 320/2
 face_y = 240/2
-top = 0
-bottom = 0
-left = 0
-right = 0
+x = 0
+y = 0
+w = 0
+h = 0
 faces = []
 def face_function():
-    global face_locations, frame, face_x, face_y, top, right, bottom, left, faces
+    global face_locations, frame, face_x, face_y, faces, x, y, w, h
     while True:
         if frame is not None:
-            #face_locations =[(100, 100+random.randint(0, 100), 100 + random.randint(0, 100), 100)]
+            x = random.randint(0, 300)
+            y = random.randint(0, 200)
+            w = 20
+            h = 20
+            faces =[(x, y, w, h)]
 
             # Find the faces and in the current frame of video
-            faces = detect_faces(frame)
+#            faces = detect_faces(frame)
             if( len(faces) > 0 ):
                 print("Found {} faces in image.".format(len(faces)))
             for face_location in faces:
-                top, right, bottom, left = face_location
+                x, y, w, h = face_location
 
                 # Face center
-                face_x = right - left
-                face_y = bottom - top
+                face_x = x + w/2
+                face_y = y + h/2
                 print("Face at {} {}".format(face_x, face_y))
                 
                 # Just look at the first face found
                 break
-        time.sleep(0.1)
+        time.sleep(1.1)
 
 # Start detection on separate thread
 print("Starting face detection.")
@@ -93,22 +113,29 @@ while True:
     frame = camera.read()
     
     # Fade to center
-    face_x -= (face_x - 320/2)/100
-    face_y -= (face_y - 240/2)/100
+#    x -= (x - 320/2)/200
+#    y -= (y - 240/2)/200
+
+    face_x = x + w/2
+    face_y = y + h/2
 
     # How far should we move to put the face into the center?
+    print( "Face {0:.2f} {0:.2f}".format( face_x, face_y ) )
     movement_x = image_center_x - face_x
     movement_y = image_center_y - face_y
+    
+    x += movement_x/200
+    y += movement_y/200
 
     # Proportional movement controller
-    k_p = 0.008
+    k_p = 0.002
     p_x = k_p * movement_x
-    p_y = k_p * movement_y
-#    print( "Moving {0:.2f} {0:.2f}".format( p_x, p_y ) )
+    p_y = k_p * movement_y*2
+    print( "Moving {0:.2f} {0:.2f}".format( p_x, p_y ) )
     
     # Move
     motors.setPWM(2, p_x)
-    motors.setPWM(1, 0) #p_y)
+    motors.setPWM(1, -p_y)
         
     # Send the PWM pulse at most every 10ms
     if time.time() > lastPWM + 0.1:
@@ -117,8 +144,8 @@ while True:
         lastPWM = time.time()            
 
     # Draw a box on faces
-    for (x, y, w, h) in faces:
-        cv2.rectangle(frame, (x, y), (x+w, y+h), (255, 0, 0), 2)
+#    for (x, y, w, h) in faces:
+    cv2.rectangle(frame, (int(x), int(y)), (int(x+w), int(y+h)), (255, 0, 0), 2)
 
     # Show frame if we have a GUI
     if display and frame is not None:
