364070e3436c126798130d5e3182642cd3ef8f1b
Rover <hatsmagee@gmail.com>
2017-08-24 02:25:44 +0000

    OpenCV face detect

--- source/look.py
+++ source/look.py
@@ -10,7 +10,9 @@ from threading import Thread
 
 # Start camera
 camera.startCamera((320, 240), 6)
-#frame = cv2.imread('face.jpg')
+
+# Create face detector
+face_cascade = cv2.CascadeClassifier('haarcascade_frontalface_default.xml')
 
 # Create window
 display = False
@@ -31,23 +33,37 @@ def timing(f):
 
 @timing
 def detect_faces(frame):
-    return face_recognition.face_locations(frame)
+    print("Detecting faces")
+    global face_cascade
+    gray = cv2.cvtColor(frame, cv2.COLOR_BGR2GRAY)
+    faces = face_cascade.detectMultiScale(gray, 1.3, 5)
+    print( faces )
+    return faces
+
+# Dlib method, slow.
+#    return face_recognition.face_locations(frame)
 
 # Detect faces loop
 frame = None
 face_locations = []
 face_x = 320/2
 face_y = 240/2
+top = 0
+bottom = 0
+left = 0
+right = 0
+faces = []
 def face_function():
-    global face_locations, frame, face_x, face_y
+    global face_locations, frame, face_x, face_y, top, right, bottom, left, faces
     while True:
         if frame is not None:
+            #face_locations =[(100, 100+random.randint(0, 100), 100 + random.randint(0, 100), 100)]
+
             # Find the faces and in the current frame of video
-            face_locations =[(100, 100+random.randint(0, 100), 100 + random.randint(0, 100), 100)]
-            #face_locations = detect_faces(frame)
-            if( len(face_locations) > 0 ):
-                print("Found {} faces in image.".format(len(face_locations)))
-            for face_location in face_locations:
+            faces = detect_faces(frame)
+            if( len(faces) > 0 ):
+                print("Found {} faces in image.".format(len(faces)))
+            for face_location in faces:
                 top, right, bottom, left = face_location
 
                 # Face center
@@ -57,8 +73,9 @@ def face_function():
                 
                 # Just look at the first face found
                 break
-        time.sleep(5.1)
-    
+        time.sleep(0.1)
+
+# Start detection on separate thread
 print("Starting face detection.")
 thread = Thread(target=face_function, args=())
 thread.start()    
@@ -74,27 +91,20 @@ while True:
 
     # Grab a single frame of video from the camera as a numpy array    
     frame = camera.read()
-
-    # Show frame if we have a GUI
-    if display and frame is not None:
-        cv2.imshow("preview", frame)
-        cv2.waitKey(1)
     
-
     # Fade to center
     face_x -= (face_x - 320/2)/100
     face_y -= (face_y - 240/2)/100
-        
+
     # How far should we move to put the face into the center?
     movement_x = image_center_x - face_x
     movement_y = image_center_y - face_y
 
-    # P controller
+    # Proportional movement controller
     k_p = 0.008
     p_x = k_p * movement_x
     p_y = k_p * movement_y
-
-    print( "Moving {0:.2f} {0:.2f}".format( p_x, p_y ) )
+#    print( "Moving {0:.2f} {0:.2f}".format( p_x, p_y ) )
     
     # Move
     motors.setPWM(2, p_x)
@@ -105,3 +115,12 @@ while True:
         motors.runPWM(1)
         motors.runPWM(2)
         lastPWM = time.time()            
+
+    # Draw a box on faces
+    for (x, y, w, h) in faces:
+        cv2.rectangle(frame, (x, y), (x+w, y+h), (255, 0, 0), 2)
+
+    # Show frame if we have a GUI
+    if display and frame is not None:
+        cv2.imshow("preview", frame)
+        cv2.waitKey(1)
