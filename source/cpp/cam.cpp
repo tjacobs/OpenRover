2916db2440e9ef4f5b7dc1d5394b442f69735f01
Tom Jacobs <tjacobs@expensify.com>
2017-10-10 11:00:22 +1100

    Cleaned up cam.h

--- source/cpp/cam.cpp
+++ source/cpp/cam.cpp
@@ -16,70 +16,67 @@ CameraReceiver *Camera::receiver_ = NULL;
 
 CameraReceiver::~CameraReceiver() {}
 
-void Camera::ControlCallback(
-    MMAL_PORT_T *port, MMAL_BUFFER_HEADER_T *buffer) {
+void Camera::ControlCallback(MMAL_PORT_T *port, MMAL_BUFFER_HEADER_T *buffer) {
+  // Print command
   fprintf(stderr, "Camera control callback cmd=0x%08x", buffer->cmd);
-
   mmal_buffer_header_release(buffer);
 }
 
-void Camera::BufferCallback(MMAL_PORT_T *port,
-                            MMAL_BUFFER_HEADER_T *buffer) {
+void Camera::BufferCallback(MMAL_PORT_T *port, MMAL_BUFFER_HEADER_T *buffer) {
   if (buffer->length) {
     if (receiver_ != NULL) {
+      // Call OnFrame
       mmal_buffer_header_mem_lock(buffer);
       receiver_->OnFrame(buffer->data, buffer->length);
       mmal_buffer_header_mem_unlock(buffer);
     }
   }
 
-  // release buffer back to the pool
+  // Release buffer back to the pool
   mmal_buffer_header_release(buffer);
 
-  // and send one back to the port (if still open)
+  // If port is still open
   if (port->is_enabled) {
     MMAL_STATUS_T status;
     MMAL_BUFFER_HEADER_T *new_buffer = mmal_queue_get(camera_pool_->queue);
 
-    // and back to the port from there.
+    // Send buffer back to the port
     if (new_buffer) {
       status = mmal_port_send_buffer(port, new_buffer);
     }
-
     if (!new_buffer || status != MMAL_SUCCESS)
-      fprintf(stderr, "Unable to return the buffer to the "
-              "camera video port\n");
+      fprintf(stderr, "Unable to return the buffer to the camera video port\n");
   }
 }
 
 bool Camera::Init(int width, int height, int fps) {
+
+  // Check resolution
   if (width & 31) {
-    fprintf(stderr, "camera: width must be multiple of 32");
+    fprintf(stderr, "Camera: Width must be multiple of 32");
     return false;
   }
-
   if (height & 15) {
-    fprintf(stderr, "camera: height must be multiple of 16");
+    fprintf(stderr, "Camera: Height must be multiple of 16");
     return false;
   }
 
+  // Create camera
   MMAL_STATUS_T status;
-
-  if ((status = mmal_component_create(MMAL_COMPONENT_DEFAULT_CAMERA, &camera_))
-      != MMAL_SUCCESS) {
-    fprintf(stderr, "cannot create mmal camera component\n");
+  if ((status = mmal_component_create(MMAL_COMPONENT_DEFAULT_CAMERA, &camera_)) != MMAL_SUCCESS) {
+    fprintf(stderr, "Camera: Cannot create mmal camera component\n");
     return false;
   }
 
   // Enable the camera, and tell it its control callback function
   status = mmal_port_enable(camera_->control, ControlCallback);
   if (status != MMAL_SUCCESS) {
-    fprintf(stderr, "cannot enable camera\n");
+    fprintf(stderr, "Camera: Cannot enable camera\n");
     return false;
   }
 
-  MMAL_PARAMETER_CAMERA_CONFIG_T cam_config = {
-    { MMAL_PARAMETER_CAMERA_CONFIG, sizeof(cam_config) }};
+  // Configure camera
+  MMAL_PARAMETER_CAMERA_CONFIG_T cam_config = { { MMAL_PARAMETER_CAMERA_CONFIG, sizeof(cam_config) }};
   cam_config.max_stills_w = width;
   cam_config.max_stills_h = height;
   cam_config.stills_yuv422 = 0;
@@ -90,11 +87,10 @@ bool Camera::Init(int width, int height, int fps) {
   cam_config.stills_capture_circular_buffer_height = 0;
   cam_config.fast_preview_resume = 0;
   cam_config.use_stc_timestamp = MMAL_PARAM_TIMESTAMP_MODE_RESET_STC;
-
   mmal_port_parameter_set(camera_->control, &cam_config.hdr);
 
+  // Configure port
   MMAL_PORT_T *video_port = camera_->output[1];
-
   video_port->format->encoding = MMAL_ENCODING_I420;
   video_port->format->encoding_variant = MMAL_ENCODING_I420;
   video_port->format->es->video.width = width;
@@ -107,7 +103,7 @@ bool Camera::Init(int width, int height, int fps) {
   video_port->format->es->video.frame_rate.den = 1;
   status = mmal_port_format_commit(video_port);
   if (status != MMAL_SUCCESS) {
-    fprintf(stderr, "cannot set video port format\n");
+    fprintf(stderr, "Camera: cannot set video port format\n");
     return false;
   }
 
@@ -115,34 +111,30 @@ bool Camera::Init(int width, int height, int fps) {
   if (video_port->buffer_num < 20)
     video_port->buffer_num = 20;
 
+  // Turn the red led on
   status = mmal_component_enable(camera_);
   if (status != MMAL_SUCCESS) {
-    fprintf(stderr, "cannot enable camera\n");
+    fprintf(stderr, "Camera: Cannot enable camera\n");
     return false;
   }
-
-  // the red LED should be on now.
-  fprintf(stderr, "video port %s: %d buffers x %d bytes\n",
-          video_port->name, video_port->buffer_num, video_port->buffer_size);
+  fprintf(stderr, "Camera: Video port %s: %d buffers x %d bytes\n", video_port->name, video_port->buffer_num, video_port->buffer_size);
 
   // Create pool of buffer headers for the output port to consume
-  camera_pool_ = mmal_port_pool_create(
-      video_port, video_port->buffer_num, video_port->buffer_size);
+  camera_pool_ = mmal_port_pool_create(video_port, video_port->buffer_num, video_port->buffer_size);
   if (!camera_pool_) {
-    fprintf(stderr, "cannot create buffer header pool for video port\n");
+    fprintf(stderr, "Camera: Cannot create buffer header pool for video port\n");
     return false;
   }
 
+  // Set up buffer callback
   if (mmal_port_enable(video_port, BufferCallback) != MMAL_SUCCESS) {
-    fprintf(stderr, "Failed to setup camera output\n");
+    fprintf(stderr, "Camera: Failed to set up camera output\n");
     return false;
   }
 
-  // set shutter speed to auto (it probably was already?)
-  if (mmal_port_parameter_set_uint32(
-          camera_->control, MMAL_PARAMETER_SHUTTER_SPEED,
-          0) != MMAL_SUCCESS) {
-    fprintf(stderr, "cannot set shutter speed\n");
+  // Set shutter speed to auto
+  if (mmal_port_parameter_set_uint32(camera_->control, MMAL_PARAMETER_SHUTTER_SPEED, 0) != MMAL_SUCCESS) {
+    fprintf(stderr, "Camera: Cannot set shutter speed\n");
     return false;
   }
 
@@ -151,43 +143,47 @@ bool Camera::Init(int width, int height, int fps) {
   for (int q = 0; q < qlen; q++) {
     MMAL_BUFFER_HEADER_T *buffer = mmal_queue_get(camera_pool_->queue);
     if (!buffer) {
-      fprintf(stderr,
-              "Unable to get a required buffer %d from pool queue\n",
-              q);
+      fprintf(stderr, "Camera: Unable to get a required buffer %d from pool queue\n", q);
     }
 
+    // Send it
     if (mmal_port_send_buffer(video_port, buffer)!= MMAL_SUCCESS) {
-      fprintf(stderr,
-              "Unable to send a buffer to camera output port (%d)\n",
-              q);
+      fprintf(stderr, "Camera: Unable to send a buffer to camera output port (%d)\n", q);
     }
   }
 
+  // Camera is set up
   return true;
 }
 
 bool Camera::StartRecord(CameraReceiver *receiver) {
+
+  // Save receiver
   receiver_ = receiver;
+
+  // Enable capturing
   MMAL_PORT_T *video_port = camera_->output[1];
-  // enable capturing
-  if (mmal_port_parameter_set_boolean(video_port, MMAL_PARAMETER_CAPTURE, 1)
-      != MMAL_SUCCESS) {
-    fprintf(stderr, "failed to start capture\n");
+  if (mmal_port_parameter_set_boolean(video_port, MMAL_PARAMETER_CAPTURE, 1) != MMAL_SUCCESS) {
+    fprintf(stderr, "Camera: Failed to start capture\n");
     return false;
   }
 
+  // We are capturing
   return true;
 }
 
 bool Camera::StopRecord() {
+
+  // Disable capturing
   MMAL_PORT_T *video_port = camera_->output[1];
-  // enable capturing
-  if (mmal_port_parameter_set_boolean(video_port, MMAL_PARAMETER_CAPTURE, 0)
-      != MMAL_SUCCESS) {
-    fprintf(stderr, "failed to stop capture\n");
+  if (mmal_port_parameter_set_boolean(video_port, MMAL_PARAMETER_CAPTURE, 0) != MMAL_SUCCESS) {
+    fprintf(stderr, "Camera: Failed to stop capture\n");
     return false;
   }
+
+  // Close receiver
   receiver_ = NULL;
 
+  // We have stopped capturing
   return true;
 }
