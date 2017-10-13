b2cc5acac489cb453007815d935e9799ffbab1b2
Rover <hatsmagee@gmail.com>
2017-10-13 03:50:13 +0000

    uWS

--- source/cpp/drive.cpp
+++ source/cpp/drive.cpp
@@ -704,22 +704,23 @@ class Driver: public CameraReceiver {
 
 int main(){
   
-    uWS::Hub h;
-
-    h.onMessage([](uWS::WebSocket<uWS::SERVER> *ws, char *message, size_t length, uWS::OpCode opCode) {
-        ws->send(message, length, opCode);
-    });
-
-    h.onHttpRequest([](uWS::HttpResponse *res, uWS::HttpRequest req, char *data, size_t length, size_t remainingBytes) {
-        res->end(const char *, size_t);
-    });
-
-    if (h.listen(3000)) {
-        h.run();
-    }
-
   // Start it up
   printf("\nStarting OpenRover.\n");
+
+  uWS::Hub h;
+  const char* message = "Websocket";
+    
+  h.onMessage([](uWS::WebSocket<uWS::SERVER> *ws, char *message, size_t length, uWS::OpCode opCode) {
+    ws->send(message, strlen(message), opCode);
+  });
+
+  h.onHttpRequest([](uWS::HttpResponse *res, uWS::HttpRequest req, char *data, size_t length, size_t remainingBytes) {
+    res->end("Woo", 3);
+  });
+
+  if (h.listen(3000)) {
+    h.run();
+  }
 
   // Start PWM output
   pi = pigpio_start(0, 0);
